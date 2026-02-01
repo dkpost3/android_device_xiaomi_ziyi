b2ab6a1e006def7979602bb6e70d2a9dc020eed9
dkpost3 <dkpost3@gmail.com>
2026-02-01 14:01:07 +0300

    ziyi: test

--- lineage_ziyi.mk
+++ lineage_ziyi.mk
@@ -10,7 +10,6 @@ $(call inherit-product, device/xiaomi/ziyi/device.mk)
 # Inherit from common lineage configuration
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-EVO_BUILD_TYPE := Official
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_HAS_UDFPS := true
 
